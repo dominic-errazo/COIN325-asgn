310a37def0378dd3d46b7a566737240d25308885
Dom Errazo <dominic.errazo@gmail.com>
2015-02-05 11:25:39 -0500

    adding first file for lab06

---
+++ lab06/Book.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Dominic
+ */
+public class Book {
+    private String _title, _author, _publisher, _copyrightDate;
+    
+    public Book(String title, String author, String publisher, String copyrightDate) {
+        
+        _title = title;
+        _author = author;
+        _publisher = publisher;
+        _copyrightDate = copyrightDate;
+    }
+    
+    public String getTitle() {
+        return _title;
+    }
+    
+    public void setTitle(String title) {
+        _title = title;
+    }
+    
+    public String getAuthor() {
+        return _author;
+    }
+
+    public void setAuthor(String author) {
+        _author = author;
+    }
+
+    public String getPublisher() {
+        return _publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        _publisher = publisher;
+    }
+
+    public String getCopyrightDate() {
+        return _copyrightDate;
+    }
+
+    public void setCopyrightDate(String copyrightDate) {
+        _copyrightDate = copyrightDate;
+    }
+    
+    
+}
